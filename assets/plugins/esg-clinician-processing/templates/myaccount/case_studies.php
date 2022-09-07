b94fbb9a21e441e9dfe2bcf0989b1961cb8b6159
Jeremy Bernabe <jeremy@interfused-inc.com>
2022-09-07 01:04:25 -0400

    save point

--- assets/plugins/esg-clinician-processing/templates/myaccount/case_studies.php
+++ assets/plugins/esg-clinician-processing/templates/myaccount/case_studies.php
@@ -5,30 +5,36 @@ $edit_step = $_GET['add_step'] ?? "default";
 
 <?php
 if ($edit_step == 'default') {
-?>
-    <section>
 
-        <?php
-        $posts = get_posts(
-            array(
-                'posts_per_page' => -1,
-                'post_status' => 'any',
-                'author'    => get_current_user_id(),
-                'post_type' => 'case_study'
-            )
-        );
-        pretty_print_r($posts);
-        if ($posts) {
-            echo sprintf(esg_get_section_format('default'), 'My Case Studies');
-            echo '<div class="cards-wrapper">';
-            foreach ($posts as $post) {
-                include('case_study/tpl-overview-case-study.php');
-            }
-            echo '</div>';
-            echo '<hr />';
+    $marketing_page = get_page_by_title('Case Study Benefits');
+    if ($marketing_page) {
+        echo '<section id="marketing">';
+        echo sprintf(esg_get_section_format('default'), $marketing_page->post_title);
+        echo '<div class="overview">' . $marketing_page->post_content . '</div>';
+        echo '</section>';
+    }
+
+
+
+    $posts = get_posts(
+        array(
+            'posts_per_page' => -1,
+            'post_status' => 'any',
+            'author'    => get_current_user_id(),
+            'post_type' => 'case_study'
+        )
+    );
+    pretty_print_r($posts);
+    if ($posts) {
+        echo sprintf(esg_get_section_format('default'), 'My Case Studies');
+        echo '<div class="cards-wrapper">';
+        foreach ($posts as $post) {
+            include('case_study/tpl-overview-case-study.php');
         }
-        ?>
-    </section>
+        echo '</div>';
+        echo '<hr />';
+    }
+?>
 
 <?php
 }
@@ -56,17 +62,16 @@ if (!isset($_GET['add_step'])) {
         <form id="<?php echo $form_tag1; ?>" method="POST" action="<?php $uri_parts[0]; ?>?mode=case_studies&add_step=2">
 
             <h2>Add New Case Study</h2>
-            <p>Don't see appropriate case study category option(s)? <a href="<?php echo $uri_parts[0] ?>?mode=add_system_case_study_category">Add New</a></p>
 
             <?php
             echo esg_generate_input_field('text', 'Case Study Title', 'post_title');
             echo esg_generate_input_field('textarea', 'Give a brief excerpt of your case study (55 words)', 'post_excerpt');
             ?>
             <?php
-
             echo taxonomy_to_checkboxes('case_study_category', 'case_study_categories', 'Case study categories', 'case_study');
-
             ?>
+            <p>Don't see appropriate case study category option(s)? <a class="btn" href="<?php echo $uri_parts[0] ?>?mode=add_system_case_study_category">Add Option(s)</a></p>
+
             <input type="hidden" name="<?php echo $form_tag1; ?>" value="1" />
             <input type="hidden" name="_wpnonce" value="<?php echo $nonce1; ?>" />
 
--- assets/plugins/esg-clinician-processing/templates/myaccount/dashboard-edit-profile.php
+++ assets/plugins/esg-clinician-processing/templates/myaccount/dashboard-edit-profile.php
@@ -8,5 +8,27 @@ $settings = array(
 
 wp_editor('', 'test_id',  $settings);
 */
-echo do_shortcode('[frontend_admin form="591"]');
+//echo do_shortcode('[frontend_admin form="591"]');
+/*
+acf_form();case_study_editor
+*/
+
+acf_form(array(
+    'form' => true,
+    'post_id'       => 'new_post',
+    'new_post'      => array(
+        'post_type'     => 'case_study',
+        'post_status'   => 'pending'
+    ),
+    'fields' => ['case_study_editor'],
+
+    'post_title'    => false,
+    'post_content'  => false,
+
+    'html_before_fields' =>  '<small>Edit your content below</small></h2>',
+    'return' => $uri_parts[0] . '?mode=case_studies&edit_success=1&id=%post_id%',
+    'updated_message' => __('this is the updated message', 'acf'),
+    'html_updated_message'  => '<div id="message" class="alert alert-info"><p>%s</p></div>',
+    'submit_value'  => __('Save')
+));
 ?>
